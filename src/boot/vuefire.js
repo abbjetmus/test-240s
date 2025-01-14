a458562606fbcb2afb81b4d9d61c258dcac8bd17
abbjetmus <jeton.mustini@gmail.com>
2025-01-14 15:25:04 +0100

    remove credentials

--- src/boot/vuefire.js
+++ src/boot/vuefire.js
@@ -4,13 +4,13 @@ import { getDatabase } from 'firebase/database'
 import { VueFire } from 'vuefire'
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAK8AJmJPmUrHjPvKl486i99SE_Wfk0R_4",
-  authDomain: "temp-projekt-220s.firebaseapp.com",
-  databaseURL: "https://temp-projekt-220s-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "temp-projekt-220s",
-  storageBucket: "temp-projekt-220s.firebasestorage.app",
-  messagingSenderId: "401787728103",
-  appId: "1:401787728103:web:3f9f7614f1dae5d1c13242"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 })
 
 const db = getDatabase(firebaseApp)
